3f659963105bc6bac178e97871d7563fc157c6c7
Ankit Sharma <ankit103510@gmail.com>
2018-03-09 14:17:52 +1000

    Add a descriptive title

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <head>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<title>INFS7901 Course List</title>
 </head>
 <body>
 
