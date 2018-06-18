5aa19928f514bb35c87edd9b7428ea26c09d7191
Ankit-Sharma-1 <37194762+Ankit-Sharma-1@users.noreply.github.com>
2018-06-18 12:45:05 +1000

    Add files via upload

---
+++ Med Review/config.php
@@ -0,0 +1,11 @@
+<?php
+
+	define("DB_SERVER", "localhost");
+	define("DB_NAME", "med_reviews");
+	// define("DB_USER", "root"); // Need this line for the local site
+	define("DB_USER", "root"); // Need this line for the hosted site
+	define("DB_PASS", "559711dfc2eae1f6"); // Need this line for the hosted site
+
+	define("DB_DSN", "mysql:host=".DB_SERVER.";dbname=".DB_NAME);
+
+?>
