3bfe0cae53758d904e8c81d8ed4c27bff7c54aa4
Ankit-Sharma-1 <37194762+Ankit-Sharma-1@users.noreply.github.com>
2018-06-18 13:11:50 +1000

    Add files via upload

---
+++ Med Review/Website/public/all_facilities/Facility/index.php
@@ -0,0 +1,16 @@
+<?php require_once('../../../private/init.php'); ?>
+
+<?php $page_title = 'Facility'; ?>
+<?php include(SHARED_PATH . '/header.php'); ?>
+<header class='facility'>
+	<?php echo $page_title ?>
+</header>
+
+<div id="content">
+	<article id="facility">
+		
+	</article>
+</div>
+
+
+<?php include(SHARED_PATH . '/footer.php'); ?>
