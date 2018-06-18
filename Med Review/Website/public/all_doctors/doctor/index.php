370116ff5715ca073417f22d2cd13ae0368936f9
Ankit-Sharma-1 <37194762+Ankit-Sharma-1@users.noreply.github.com>
2018-06-18 13:13:25 +1000

    Add files via upload

---
+++ Med Review/Website/public/all_doctors/doctor/index.php
@@ -0,0 +1,16 @@
+<?php require_once('../../../private/init.php'); ?>
+
+<?php $page_title = 'Doctor'; ?>
+<?php include(SHARED_PATH . '/header.php'); ?>
+<header class='doctor'>
+	<?php echo $page_title ?>
+</header>
+
+<div id="content">
+	<article id="doctor">
+		
+	</article>
+</div>
+
+
+<?php include(SHARED_PATH . '/footer.php'); ?>
