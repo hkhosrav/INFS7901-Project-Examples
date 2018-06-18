393550aee7ae83edd05989f684eac74e60f41ef5
Ankit-Sharma-1 <37194762+Ankit-Sharma-1@users.noreply.github.com>
2018-06-18 13:08:54 +1000

    Add files via upload

---
+++ Med Review/Website/public/all_pharmacists/index.php
@@ -0,0 +1,13 @@
+<?php require_once('../../private/init.php'); ?>
+
+<?php $page_title = 'Doctors'; ?>
+<?php include(SHARED_PATH . '/header.php'); ?>
+
+<div id="content">
+	<article id="doctors">
+		
+	</article>
+</div>
+
+
+<?php include(SHARED_PATH . '/footer.php'); ?>
