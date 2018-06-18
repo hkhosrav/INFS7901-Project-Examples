123f53f090709c69965876f292bf68254da9999c
Ankit-Sharma-1 <37194762+Ankit-Sharma-1@users.noreply.github.com>
2018-06-18 12:49:08 +1000

    Add files via upload

---
+++ Med Review/Website/private/shared/footer.php
@@ -0,0 +1,8 @@
+<footer>
+  <?php 
+  date_default_timezone_set('Australia/Brisbane');
+  echo date("l jS \of F Y"); 
+  ?>
+</footer>
+</body>
+</html>
---
+++ Med Review/Website/private/shared/landing.php
@@ -0,0 +1,19 @@
+<!doctype html>
+
+<html lang="en">
+
+<head>
+	<title><?php echo $page_title; ?></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" charset="utf-8">
+	<link rel="stylesheet" media="all" href="<?php echo WWW_ROOT . '/stylesheets/home.css'; ?>" />
+
+</head>
+
+<body>
+
+	<nav class="landing">
+		<img src="<?php echo WWW_ROOT. '/img/longlogo.png'; ?>" style="height:130px; display: block;
+    margin: 0 auto;">
+	</nav>
+
+
